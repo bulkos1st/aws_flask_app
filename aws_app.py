1862e2435def45b591a457916e1f32e2837e83e8
bulkos1st <65762185+bulkos1st@users.noreply.github.com>
2025-01-20 17:45:35 +0100

    updated app with aws doc

--- aws_app.py
+++ aws_app.py
@@ -3,12 +3,46 @@ import json
 import os
 import requests
 
-from flask import Flask
 from flask import Flask, jsonify, render_template, render_template_string, url_for, redirect, flash, g
 import boto3
 
 
+def get_instance_document():
+    try:
+        r = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document")
+        if r.status_code == 401:
+            token=(
+                requests.put(
+                    "http://169.254.169.254/latest/api/token", 
+                    headers={'X-aws-ec2-metadata-token-ttl-seconds': '21600'}, 
+                    verify=False, timeout=1
+                )
+            ).text
+            r = requests.get(
+                "http://169.254.169.254/latest/dynamic/instance-identity/document",
+                headers={'X-aws-ec2-metadata-token': token}, timeout=1
+            )
+        r.raise_for_status()
+        return r.json()
+    except:
+        print(" * Instance metadata not available")
+        return { "availabilityZone" : "us-fake-1a",  "instanceId" : "i-fakeabc" }
+
+
+
 app = Flask(__name__)
+
+doc = get_instance_document()
+availablity_zone = doc["availabilityZone"]
+instance_id = doc["instanceId"]
+
+
+@app.before_request
+def before_request():
+    "Set up globals referenced in jinja templates"
+    g.availablity_zone = availablity_zone
+    g.instance_id = instance_id
+
 
 @app.route("/")
 def home():
